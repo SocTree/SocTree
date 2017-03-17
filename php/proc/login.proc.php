3951ce88afd9dc569c914a263ecde277f84b7244
rogerfus <10000712.joan23@fje.edu>
2017-03-17 18:48:26 +0100

    encriptación comentada

--- php/proc/login.proc.php
+++ php/proc/login.proc.php
@@ -3,7 +3,7 @@ extract($_REQUEST); //Recibimos $email i $password
 
 include('./conexion/conexion.php');
 
-//Encriptamos la password
+//Encriptamos la password con hash sha512
 $password = hash('sha512',$password);
 
 $sql = "SELECT * FROM tbl_usuari WHERE usu_email = $email AND usu_password = $password;";
