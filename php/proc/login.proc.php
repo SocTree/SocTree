6179fdae476f43ad567aa280ca9c5e8058773782
rogerfus <10000712.joan23@fje.edu>
2017-03-17 18:31:22 +0100

    Login proc terminado a falta de testear con bbdd

--- php/proc/login.proc.php
+++ php/proc/login.proc.php
@@ -8,4 +8,17 @@ $password = hash('sha512',$password);
 
 $sql = "SELECT * FROM tbl_usuari WHERE usu_email = $email AND usu_password = $password;";
 
+$resultado=mysqli_query($conexion, $sql);
+	if (mysqli_num_rows($resultado) != 0 ) {
+		while ($usuario = mysqli_fetch_array($resultado)) {
+			$usu_email = $usuario['usu_email'];
+			$usu_password = $usuario['usu_password'];
+			if ($email == $usu_email && $password == $usu_password) {
+				$_SESSION['usu_id'] = $usuario['usu_id'];
+				header('Location:../../index.php');
+			}
+		}
+	}
+	
+                       
 ?>
