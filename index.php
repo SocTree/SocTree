4a4a51daee9d1ade5a330372eead2eba20181e0e
edhuchacaliaza <93191.joan23@fje.edu>
2017-03-21 17:27:08 +0100

    index chusquero

---
+++ index.php
@@ -0,0 +1,7 @@
+<!-- Header -->
+<?php include("php/main/header/header.php"); ?>
+<!-- fin header -->
+	<div class="jumbotron">
+		<img src="img/redes/fondo.jpg" style="width: 100%;">
+	</div>
+</div>	
---
+++ php/main/header/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<style type="text/css">
+		.menu {
+			float: left;
+			margin: 2%;
+			background-color: green;
+			padding: 1.5%;
+		}
+	</style>
+</head>
+<body>
+<div class="container">
+<header>	
+	<div class="row">
+	<br/>
+		<div class="col-sm-6">
+			<img src="img/web/logo/gif.gif" width="250">
+		</div>
+		<div class="col-sm-6">
+			<div class="menu">ecoGeolocalizació</div>
+			<div class="menu">ecoEvents</div>
+			<div class="menu">ecoBlog</div>
+			<div class="menu">ecoBotiga</div>
+			<div class="menu">ecoLogin</div>
+		</div>
+	</div>	
+</header>
+<br/>
