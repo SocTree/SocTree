9cc067a1f900b0e524912396c72924fe69047753
10000585 <10000585.joan23@fje.edu>
2017-03-24 19:37:05 +0100

    Eventos

--- php/main/body/eventos.php
+++ php/main/body/eventos.php
@@ -3,15 +3,26 @@
 <head>
 	<title></title>
 	<?php include("../head.php");?>
+	<link rel="stylesheet" type="text/css" href="../../../css/eventos.css">
 </head>
 <body>
 <!-- Header -->
 <?php include("../header/header.php");?>
 <!-- fin header -->
-<div class="container contenido_index">
-<!-- verEventos -->
-<?php include("verEventos.php");?>
-<!-- fin verEventos -->
+<div class="contenido_index">
+	<div class="row">
+		<div class="col-md-12 event_main">
+			<div class="col-md-6">
+				<p>aldsfasd</p>
+			</div>
+			<div class="col-md-6">
+				<p>aldsfasd</p>
+			</div>
+		</div>
+	</div>
+<!-- verEventos
+<?php //include("verEventos.php");?>
+fin verEventos -->
 </div>
 <!-- Footer -->
 <?php include("../footer/footer.php");?>
