22d0a907023a946434fadebef85c6a35b07ad5f1
10000585 <10000585.joan23@fje.edu>
2017-03-24 19:51:56 +0100

    Eventos

--- php/main/body/eventos.php
+++ php/main/body/eventos.php
@@ -20,6 +20,12 @@
 			</div>
 		</div>
 	</div>
+	<div class="row">
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at sapien leo. Nulla eget nunc rutrum, dictum sem nec, facilisis est. Nam dictum massa eget tortor venenatis, id ultricies massa finibus. Cras vulputate dignissim turpis, imperdiet venenatis erat dignissim vitae. Phasellus sit amet arcu at magna finibus imperdiet. Cras finibus molestie dolor ac imperdiet. Pellentesque ultricies ipsum et consectetur consequat. Pellentesque molestie tempor sapien, eu fermentum libero suscipit at. Donec vestibulum faucibus ex, quis gravida massa porta consectetur. Praesent et fringilla mi, eu elementum ipsum. Nam sit amet hendrerit enim. Aliquam erat volutpat.</p>
+
+		<p>Donec nunc odio, vulputate a elit ullamcorper, gravida hendrerit tortor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut sed justo ac nisi dignissim fringilla. Integer tincidunt tellus vitae nibh vehicula, vitae mattis nulla lobortis. Aenean volutpat quam ac diam mollis egestas. Aliquam at elit facilisis, hendrerit lectus ac, auctor dolor. Aenean interdum sollicitudin augue at lacinia. Nam ut sagittis est.</p>
+
+	</div>
 <!-- verEventos
 <?php //include("verEventos.php");?>
 fin verEventos -->
