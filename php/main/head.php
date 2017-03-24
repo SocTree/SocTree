84b33fe6560b1e7b4befdc0a74b89c93b2050fc1
10000585 <10000585.joan23@fje.edu>
2017-03-24 17:10:34 +0100

    Orden head
    Ordenamos las carpetas y ponemos el head en su sitio

---
+++ php/main/head.php
@@ -0,0 +1,9 @@
+	<!-- PARTE DEL HEAD RELATIVA AL HEADER -->
+	<link rel="stylesheet" type="text/css" href="../../../css/bootstrap.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  	<link rel="stylesheet" href="http://fontawesome.io/assets/font-awesome/css/font-awesome.css">
+	<link rel="stylesheet" type="text/css" href="../../../css/header.css">
+	<link rel="stylesheet" type="text/css" href="../../../css/footer.css">
+	<link rel="stylesheet" type="text/css" href="../../../css/body.css">
+	
--- php/main/header/header.php
+++ php/main/header/header.php
@@ -1,22 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-	<!-- PARTE DEL HEAD RELATIVA AL HEADER -->
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  	<link rel="stylesheet" href="http://fontawesome.io/assets/font-awesome/css/font-awesome.css">
-	<link rel="stylesheet" type="text/css" href="css/header.css">
-	<link rel="stylesheet" type="text/css" href="css/footer.css">
-	<link rel="stylesheet" type="text/css" href="css/body.css">
-	<!-- PARTE DEL HEAD RELATIVA AL SLIDE -->
-	<link rel="stylesheet" href="css/slider.css" type="text/css">
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
-	<script src="js/jquery.flexslider-min.js"></script>
-	<style>.flex-container{position: relative;margin: 0 auto; top: 0%; padding: 50px;} </style> 
-</head>
-<body>
+
 <div class="menufijo">
  	<div class="container">	
 	<div class="row">
