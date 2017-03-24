1fedb834d51ac1b1185ac76c1c15e11727465fc7
10000585 <10000585.joan23@fje.edu>
2017-03-24 16:28:47 +0100

    Revisión cambios

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!-- Header -->
 <?php include("php/main/header/header.php");?>
 <!-- fin header -->
-<div style="margin-top: 132px;">
+<div style="margin-top: 132px; ">
 <div class="container">
 <!-- Inicio slider -->
 <div class="jumbotron">
@@ -10,12 +10,10 @@
 			<ul class="slides">
 				<li>
 					<a href="#"><img src="img/web/slider/slide1.jpg" /></a>
-				</li>
-				
+				</li>				
 				<li>
 					<img src="img/web/slider/slide2.jpg" />
-				</li>
-				
+				</li>				
 				<li>
 					<img src="img/web/slider/slide3.jpg" />
 				</li>
@@ -24,6 +22,54 @@
 	</div>
 </div>
 <!-- fin slider -->
+
+<section class="info">
+	<img src="img/web/logo/1.png" width="250"><p><i>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac odio odio. Pellentesque bibendum libero ac cursus fermentum. Praesent non mi accumsan, fermentum tellus vitae, faucibus arcu. Suspendisse at vehicula ligula. Donec tempor velit dui, non molestie odio cursus ac."</i></p>	
+</section>
+<br>
+
+<section class="jobs">
+	<div class="col-sm-6">
+		<div class="jobs-content">
+			<img src="img/web/icon/png/geo.png">
+			<h3>Llocs d'interès</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dolor turpis, condimentum id consectetur a, tristique in lorem. Ut posuere.</p>
+		</div>	
+	</div>
+	
+	<div class="col-sm-6">
+		<div class="jobs-content">
+			<img src="img/web/icon/png/event.png">
+			<h3>Events</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dolor turpis, condimentum id consectetur a, tristique in lorem. Ut posuere.</p>
+		</div>	
+	</div>
+	
+	<div class="col-sm-6">
+		<div class="jobs-content">
+			<img src="img/web/icon/png/tips.png">
+			<h3>Tips al Blog</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dolor turpis, condimentum id consectetur a, tristique in lorem. Ut posuere.</p>
+		</div>	
+	</div>
+	
+	<div class="col-sm-6">
+		<div class="jobs-content">
+			<img src="img/web/icon/png/premis.png">
+			<h3>Premis pel medi</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dolor turpis, condimentum id consectetur a, tristique in lorem. Ut posuere.</p>
+		</div>	
+	</div>
+
+</section>
+<br>
+</div>
+<section class="patrocinadores">
+	hola
+</section>
+<div class="container">
+
+
 	<!-- modal que se usará luego NO BORRAR!! -->
 	<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
 	<div class="modal fade" id="myModal" role="dialog"><?php include_once("php/main/body/agregarEvento.php"); ?></div>
--- php/main/header/header.php
+++ php/main/header/header.php
@@ -9,6 +9,7 @@
   	<link rel="stylesheet" href="http://fontawesome.io/assets/font-awesome/css/font-awesome.css">
 	<link rel="stylesheet" type="text/css" href="css/header.css">
 	<link rel="stylesheet" type="text/css" href="css/footer.css">
+	<link rel="stylesheet" type="text/css" href="css/body.css">
 	<!-- PARTE DEL HEAD RELATIVA AL SLIDE -->
 	<link rel="stylesheet" href="css/slider.css" type="text/css">
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
@@ -16,15 +17,14 @@
 	<style>.flex-container{position: relative;margin: 0 auto; top: 0%; padding: 50px;} </style> 
 </head>
 <body>
-<!-- esta linea estropeaba la ventana modal -->
-<!--<div style="position: fixed;width: 100%;top: 0;background-color: white;z-index: 1000">-->
+<!-- <div style="position: fixed;width: 100%;top: 0;background-color: white;z-index: 1000"> -->
 <div class="container">	
 	<div class="row">
 	<br/>
-		<div class="col-sm-5 col-xs-4">
+		<div class="col-sm-5">
 			<img src="img/web/logo/gif.gif" width="200">
 		</div>
-		<div class="col-sm-7 col-xs-8">
+		<div class="col-sm-7">
 			<div class="col-sm-offset-4 col-sm-8">
 				<div class="nombre">
 				<?php  
@@ -33,43 +33,29 @@
 				}else{
 					include 'php/includes/visualizarPermisivo.php';
 				}
-				
-
 				if (isset($usu))  {
 					echo "<a href=''>";
 					echo $nom;
 					echo  ', '. $cognom .'</a>&nbsp;&nbsp;|&nbsp;&nbsp;'; 
 					?>
-
 					<a href="#"><i class="fa fa-shopping-basket" aria-hidden="true"></i></a>&nbsp;&nbsp;
 					<a href="#"><i class="fa fa-user fa-lg" aria-hidden="true"></i></a>&nbsp;&nbsp;
 					<a href='php/proc/destroysesion.proc.php' ><i class="fa fa-power-off fa-lg" aria-hidden="true"></i></a>
-
 				<?php 
 				}else{ ?>
 				<button type="button" class="btn btn-success" data-toggle="modal" data-target="#modal-1">Inicia sessió</button>
-		        <div class="modal fade" id="modal-1" role="dialog"><?php 
-		        if (file_exists('../body/login2.php')) {
-					include '../body/login2.php';
-				}else{
-					include 'php/main/body/login2.php';
-				}
-		         ?></div>
-		           
-					
+		        <div class="modal fade" id="modal-1" role="dialog"><?php include_once("php/main/body/login2.php"); ?></div>					
 					<?php }
 					?>
-
-					 
-					
-				</div>				
-			</div>
-			<div class="col-sm-12">
+				</div>
+				<div class="col-sm-12">
 				<a href="#Blog"><div class="menu">Blog</div></a>
 				<a href="#Premis"><div class="menu">Premis</div></a>
 				<a href="#Events"><div class="menu">Events</div></a>
 				<a href="#Llocsdinteres"><div class="menu">Llocs d'Interès</div></a>		
+			</div>				
 			</div>
+			
 		</div>
 	</div>
 </div>
