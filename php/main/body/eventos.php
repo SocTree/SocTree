01c688f5f25b4489e66c9a3b91ef4941e3fc5f09
10000585 <10000585.joan23@fje.edu>
2017-03-27 15:56:12 +0200

    Eventos

--- php/main/body/eventos.php
+++ php/main/body/eventos.php
@@ -10,21 +10,25 @@
 <?php include("../header/header.php");?>
 <!-- fin header -->
 <div class="contenido_index">
+	<!-- Este es el divisor verde superior -->
 	<div class="row">
 		<div class="col-md-12 event_main">
 			<div class="col-md-6">
-				<p>aldsfasd</p>
+				<div class=""></div>
+				<img src="../../../img/web/icon/png/event-nom.png">
 			</div>
 			<div class="col-md-6">
 				<p>aldsfasd</p>
 			</div>
 		</div>
 	</div>
+	<!-- Fin divisor verde superior -->
 	<div class="row">
+		<div class="col-md-12">
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at sapien leo. Nulla eget nunc rutrum, dictum sem nec, facilisis est. Nam dictum massa eget tortor venenatis, id ultricies massa finibus. Cras vulputate dignissim turpis, imperdiet venenatis erat dignissim vitae. Phasellus sit amet arcu at magna finibus imperdiet. Cras finibus molestie dolor ac imperdiet. Pellentesque ultricies ipsum et consectetur consequat. Pellentesque molestie tempor sapien, eu fermentum libero suscipit at. Donec vestibulum faucibus ex, quis gravida massa porta consectetur. Praesent et fringilla mi, eu elementum ipsum. Nam sit amet hendrerit enim. Aliquam erat volutpat.</p>
 
 		<p>Donec nunc odio, vulputate a elit ullamcorper, gravida hendrerit tortor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut sed justo ac nisi dignissim fringilla. Integer tincidunt tellus vitae nibh vehicula, vitae mattis nulla lobortis. Aenean volutpat quam ac diam mollis egestas. Aliquam at elit facilisis, hendrerit lectus ac, auctor dolor. Aenean interdum sollicitudin augue at lacinia. Nam ut sagittis est.</p>
-
+		</div>
 	</div>
 <!-- verEventos
 <?php //include("verEventos.php");?>
