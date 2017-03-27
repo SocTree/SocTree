c6252227fb2d2d2871cef4008bd8131cb5ea1759
Marc Petit <1001489.joan23@fje.edu>
2017-03-27 16:23:49 +0200

    subiendo puntos interes

---
+++ agregarEvento.proc.php
@@ -0,0 +1,29 @@
+<?php session_start();
+extract($_REQUEST);
+// Con el extract recojemos las siguientes variables:
+// 	$eve_name
+// 	$eve_descripcio
+// 	$eve_tipus
+// 	$eve_data
+// 	$eve_localitzacio
+// 	$eve_estat
+// 	$eve_min_part
+// 	$eve_max_part
+
+//recojemos una variable $usu_id contenedora del id del usuario de la sesion:
+// $usu_id = $_SESSION['usu_id'];
+$eve_estat = "finalitzat";
+//usu_id temporal para pruebas:
+$usu_id = 1;
+
+include '../conexio/conexio.php';
+$sql = "INSERT INTO `tbl_events` (`eve_id`, `eve_nom`, `eve_descripcio`, `eve_tipus`, `eve_data`, `eve_localitzacio`, `usu_id`, `eve_estat`, `eve_min_part`, `eve_max_part`, `eve_actual_part`) VALUES (NULL, '$eve_name', '$eve_descripcio', '$eve_tipus', '$eve_data', '$eve_localitzacio', '$usu_id', '$eve_estat', '$eve_min_part', '$eve_max_part', NULL);";
+// echo "$sql";
+mysqli_query($conexion, $sql);
+
+//De momento redirige al índice, pendiente de cambio.
+header('Location:index.html');
+
+
+
+ ?>
