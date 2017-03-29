73f9189886e84b3bdab12104311bf402f89f1432
10000585 <10000585.joan23@fje.edu>
2017-03-29 20:27:03 +0200

    PROC

--- php/proc/agregarEvento.proc.php
+++ php/proc/agregarEvento.proc.php
@@ -14,11 +14,11 @@ extract($_REQUEST);
 
 //recojemos una variable $usu_id contenedora del id del usuario de la sesion:
 // $usu_id = $_SESSION['usu_id'];
-$eve_estat = "inactiu";
+$eve_estat = "actiu";
 //usu_id temporal para pruebas:
 $usu_id = $usu;
 $activar="http://soctree.esy.es/php/proc/activar?eve_name=$eve_name";
-$email = "titulo: ".$eve_name."<br>lloc: ".$eve_localitzacio."<br>fecha: ".$eve_data."<br>min: ".$eve_min_part."<br>max: ".$eve_max_part"<br>descripcio: ".$eve_descripcio."<br><br>activar: ".$activar;
+$email = "titulo: ".$eve_name."<br>lloc: ".$eve_localitzacio."<br>fecha: ".$eve_data."<br>min: ".$eve_min_part."<br>max: ".$eve_max_part."<br>descripcio: ".$eve_descripcio."<br><br>activar: ".$activar;
 include '../conexio/conexio.php';
 $sql = "INSERT INTO `tbl_events` ( `eve_nom`, `eve_descripcio`, `eve_tipus`, `eve_data`, `eve_localitzacio`, `usu_id`, `eve_estat`, `eve_min_part`, `eve_max_part`) VALUES ('$eve_name', '$eve_descripcio', '$eve_tipus', '$eve_data', '$eve_localitzacio', $usu_id, '$eve_estat', $eve_min_part, $eve_max_part);";
  echo "$sql";
