040b099d58eebbe84fc270a2eb0f7a99d805ec2e
rogerfus <10000712.joan23@fje.edu>
2017-03-29 18:02:05 +0200

    cron terminado

--- php/proc/cron.proc.php
+++ php/proc/cron.proc.php
@@ -9,7 +9,16 @@ $resultado=mysqli_query($conexion, $sqlTokens);
 	if (mysqli_num_rows($resultado) != 0 ) {
 		while ($usuario = mysqli_fetch_array($resultado)) {
 			$usu_id = $usuario['usu_id'];
-			
+			$sqlDarTokens = "SELECT * FROM `tbl_moneder` WHERE `tbl_moneder`.`usu_id` = $usu_id";
+			$resultadoDar=mysqli_query($conexion, $sqlDarTokens);
+			if (mysqli_num_rows($resultadoDar) != 0 ) {
+				while ($darTokens = mysqli_fetch_array($resultadoDar)) {
+					$tokensActuales = $darTokens['mon_quantitat'];
+				}
+			}
+					$tokensActuales = $tokensActuales + 5;
+			$sqlDarTokens = "UPDATE `tbl_moneder` SET `mon_quantitat` = '$tokensActuales' WHERE `tbl_moneder`.`usu_id` = $usu_id;";
+			mysqli_query($conexion, $sqlDarTokens);
 		}
 	}
 
