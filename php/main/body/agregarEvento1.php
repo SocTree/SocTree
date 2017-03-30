f75e6f992583a1d8f928a2ad53ed320098ccbee6
10000585 <10000585.joan23@fje.edu>
2017-03-30 09:40:42 +0200

    eventos
    Mejoras

--- php/main/body/agregarEvento1.php
+++ php/main/body/agregarEvento1.php
@@ -18,29 +18,45 @@ include '../../includes/visualizarRestrictivo.php';
 			<div class="modal-body">
 			
 			<form name="anadirEvento" action="../../proc/agregarEvento.proc.php">
-				Nom de l'event:
-				<input type="textArea" name="eve_name"><br><br>
+				<i>Afegir un event es molt fàcil. Engresca els altres! Disfruta! Comparteix!</i><br><br>
+				<label> Nom de l'event:<br>
+				<input type="textArea" name="eve_name">
+				</label>
+				<br>
+				<label>
 				Descripció:<br>
-				<textarea name="eve_descripcio" maxlength="5000"></textarea><br><br>
-				Tipus:
+				<textarea name="eve_descripcio" maxlength="5000"></textarea>
+				</label>
+				<br>
+				<label>
+				Tipus:<br>
 				<select name="eve_tipus">
 				  <option value="esport">Esport</option>
 				  <option value="gastronòmic">Gastronòmic</option>
 				  <option value="3R">3R</option>
 				  <option value="diy">DIY</option>
 				  <option value="solidari" selected>Solidari</option>
-				</select><br><br>
-				Dia de l'event:
-				<input type="date" name="eve_data"><br><br>
-				Lloc:
-				<input type="text" name="eve_localitzacio"><br><br>
+				</select>
+				</label>
+				<br>
+				<label>
+				Dia de l'event:<br>
+				<input type="date" name="eve_data">
+				</label>
+				<br>
+				<label>
+				Lloc:<br>
+				<input type="text" name="eve_localitzacio">
+				</label>
+				<br>
 				<?php //echo "<input type='hidden' name='usu_id' value='$_SESSION['usu_id']><br><br>" ?>
 
 				<!-- Aun no se si el estado lo controlamos nosotros o el dueño del evento. Lo pongo por si acaso -->
-				
-				Participants:
-					<li>Mínim:<input type="number" name="eve_min_part"><br><br></li>
-					<li>Màxim:<input type="number" name="eve_max_part"><br><br></li>
+				<label>
+				Participants:<br>
+					<li><i>Mínim:</i><input type="number" name="eve_min_part"><br><br></li>
+					<li><i>Màxim:</i><input type="number" name="eve_max_part"><br><br></li>
+				</label>
 		</div>
 		<div class="modal-footer">
 				<input type="submit" class="btn btn-success" name="enviar">
